f6558327abb3058e3ba5a65cffb500e7f36af57c
KevKev30 <kevin.nguyenoanh30@gmail.com>
2026-04-22 14:55:25 +0200

    ajout de quelques fonctions

--- utilisateurs.js
+++ utilisateurs.js
@@ -2,6 +2,7 @@ function supprimerMsg(name){
     name.forEach(function(em){em.remove();});
 }
 
+
 function afficherErreur(element, message) {
     let parent = element.parentNode;
     let anciens = parent.querySelectorAll("em");
@@ -15,6 +16,7 @@ function afficherErreur(element, message) {
     }
 }
 
+
 function estVide(name){
     let elt = document.getElementsByName(name)[0];
 
@@ -24,4 +26,51 @@ function estVide(name){
     }
     afficherErreur(elt, "");
     return false;
+}
+
+
+function verifierMdp() {
+    let mdp = document.getElementsByName("password")[0];
+    if (mdp.value.trim() === "") {
+        afficherErreur(mdp, "<br>Le champ est vide.");
+        return false;
+    } else if (mdp.value.length < 6) {
+        afficherErreur(mdp, "<br>Le mot de passe doit contenir au moins 6 caractères.");
+        return false;
+    } else {
+        afficherErreur(mdp, "");
+        return true;
+    }
+}
+
+
+function visibilite() {
+    let champ = document.getElementsByClassName("champ")[0];
+    if (champ.type === "password"){
+        champ.type = "text";
+    }
+    else{
+        champ.type = "password";
+    }
+}
+
+
+function estEmail(email) {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+    return regex.test(email);
+}
+
+
+function verifierEmail() {
+    let email = document.getElementsByName("email")[0];
+    if (email.value.trim() === "") {
+        afficherErreur(email, "<br>Le champ est vide.");
+        return false;
+    } else if (!estEmail(email.value)) {
+        afficherErreur(email, "<br>L'email n'est pas valide.");
+        return false;
+    } else {
+        afficherErreur(email, "");
+        return true;
+    }
 }
